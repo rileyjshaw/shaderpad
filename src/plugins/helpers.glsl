d64f187afb27265b65050fb6cf2a0b3213dd0d97
Riley Shaw <rileyjshaw@gmail.com>
2025-11-24 12:08:59 -0500

    Fix fitCover/fitContain function name swap

--- src/plugins/helpers.glsl
+++ src/plugins/helpers.glsl
@@ -3,13 +3,13 @@ uniform vec2 u_resolution;
 // Apply aspect ratio correction (object-fit: contain)
 vec2 fitContain(vec2 uv, vec2 textureSize) {
 	vec2 scale = u_resolution.xy * textureSize.yx / (u_resolution.yx * textureSize.xy);
-	return (uv - 0.5) * min(scale, vec2(1.0)) + 0.5;
+	return (uv - 0.5) * max(scale, vec2(1.0)) + 0.5;
 }
 
 // Apply aspect ratio correction (object-fit: cover)
 vec2 fitCover(vec2 uv, vec2 textureSize) {
 	vec2 scale = u_resolution.xy * textureSize.yx / (u_resolution.yx * textureSize.xy);
-	return (uv - 0.5) * max(scale, vec2(1.0)) + 0.5;
+	return (uv - 0.5) * min(scale, vec2(1.0)) + 0.5;
 }
 
 // Get the array index for a history texture
